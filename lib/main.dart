edf76c9d97348e53d24a37da4c0956aa35560dea
Mahmutihsandal <mahmutihsandal@gmail.com>
2024-04-08 16:11:18 +0300

    inital commit

---
+++ lib/main.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'package:flutter/services.dart';
+import 'package:flutter_internet_signal/flutter_internet_signal.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int? _mobileSignal;
+  int? _wifiSignal;
+  int? _wifiSpeed;
+  String? _version;
+
+  final _internetSignal = FlutterInternetSignal();
+
+  @override
+  void initState() {
+    super.initState();
+    _getPlatformVersion();
+  }
+
+  Future<void> _getPlatformVersion() async {
+    try {
+      _version = await _internetSignal.getPlatformVersion();
+    } on PlatformException {
+      if (kDebugMode) print('Error get Android version.');
+      _version = null;
+    }
+    setState(() {});
+  }
+
+  Future<void> _getInternetSignal() async {
+    int? mobile;
+    int? wifi;
+    int? wifiSpeed;
+    try {
+      mobile = await _internetSignal.getMobileSignalStrength();
+      wifi = await _internetSignal.SignalStrength();
+      wifiSpeed = await _internetSignal.getWifiLinkSpeed();
+    } on PlatformException {
+      if (kDebugMode) print('Error get internet signal.');
+    }
+    setState(() {
+      _mobileSignal = mobile;
+      _wifiSignal = wifi;
+      _wifiSpeed = wifiSpeed;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Internet Signal Example'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('On Version: $_version \n'),
+              Text('Mobile signal: ${_mobileSignal ?? '--'} [dBm]\n'),
+              Text('Wifi signal: ${_wifiSignal ?? '--'} [dBm]\n'),
+              Text('Wifi speed: ${_wifiSpeed ?? '--'} Mbps\n'),
+              ElevatedButton(
+                onPressed: _getInternetSignal,
+                child: const Text('Get internet signal'),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
